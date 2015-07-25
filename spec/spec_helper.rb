2c5f6f2d1c64862bd51c0fa5fb48be5367cc125e
Dustin Zeisler <dustin@zeisler.net>
2015-07-24 22:13:48 -0700

    set up code climate code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'dissociated_introspection'
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
