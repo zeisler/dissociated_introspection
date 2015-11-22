0da0e3e4a1c8bb49e71f1f445b68fd8c2eccd343
Dustin Zeisler <dustin@zeisler.net>
2015-11-22 13:57:03 -0800

    Add yard doc Remove Dead code

--- lib/dissociated_introspection/inspection.rb
+++ lib/dissociated_introspection/inspection.rb
@@ -3,46 +3,57 @@ require 'ostruct'
 module DissociatedIntrospection
   class Inspection
 
+    # @param file [File]
+    # @optional parent_class_replacement [Symbol]
     def initialize(file:, parent_class_replacement: :RecordingParent)
       @file                     = file
       @parent_class_replacement = parent_class_replacement
     end
 
+    # @return [Class]
     def get_class
       @get_class ||= _get_class
     end
 
-    def class_macros(type=nil)
-      # FIXME
-      return get_class.__missing_class_macros__ if type.nil?
+    # @return [Array]
+    def class_macros
+      get_class.__missing_class_macros__
     end
 
+    # @return [Array<Module>]
     def extended_modules
       find_class_macro_by_type(:extend) { |a| add_method_name_wo_parent a.first }
     end
 
+    # @return [Array<Module>]
     def included_modules
       find_class_macro_by_type(:include) { |a| add_method_name_wo_parent a.first }
     end
 
+    # @return [Array<Module>]
     def prepend_modules
       find_class_macro_by_type(:prepend) { |a| add_method_name_wo_parent a.first }
     end
 
+    # @return [Hash{String => Module}]
     def missing_constants
       get_class.__missing_constants__
     end
 
+    # @optional type [Module, Class]
+    # @return [Array<Symbol>]
     def locally_defined_constants(type=nil)
       consts = get_class.constants - get_class.__missing_constants__.keys - [:BasicObject]
       return consts unless type
       consts.select{ |c| get_class.const_get(c).is_a?(type)}
     end
 
+    # @return [DissociatedIntrospection::RubyClass]
     def parsed_source
       @parsed_source ||= RubyClass.new(source: file.read)
     end
 
+    # @return [Module]
     def sandbox_module
       @sandbox_module ||= Module.new
     end
