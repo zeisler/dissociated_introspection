da47ff227c9dd2dae93cdd5720afa8e12e7cc90e
Dustin Zeisler <dustin@zeisler.net>
2017-06-23 12:48:27 -0700

    Remove code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'dissociated_introspection'
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require "simplecov"
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
-end
 
 RSpec.configure do |c|
   c.disable_monkey_patching!
